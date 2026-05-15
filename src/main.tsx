85304c4e4f6f093a40510801cefcc1b1dd102058
mwangi-george <mwangigeorge648@gmail.com>
2026-05-15 12:36:18 +0300

    wrap app under base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
+const basePath = import.meta.env.VITE_BASE_FRONTEND_PATH as string;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter basename={basePath}>
+          <App />
+      </BrowserRouter>
   </StrictMode>,
 )
